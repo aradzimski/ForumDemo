4f25a26a265ce104956c03c41b362e33aa403483
aradzimski <adrianradzimski@gmail.com>
2021-05-22 14:54:35 +0200

    Created basic repository class for Forums

---
+++ Repositories/ForumRepository.cs
@@ -0,0 +1,27 @@
+﻿using ForumDemo.Data;
+using ForumDemo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumDemo.Repositories
+{
+    public class ForumRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ForumRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Forum>> GetAll()
+        {
+            var result = await _dbContext.Forums.ToListAsync();
+
+            return result;
+        }
+    }
+}
