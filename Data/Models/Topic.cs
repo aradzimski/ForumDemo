baeefe586bf59b2631038721bdb4ad80275e60f6
aradzimski <adrianradzimski@gmail.com>
2021-05-22 11:08:26 +0200

    Created topic model class

---
+++ Data/Models/Topic.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumDemo.Data.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+        public virtual IdentityUser User { get; set; }
+        public virtual Forum Forum { get; set; }
+
+        public virtual IEnumerable<Post> Posts { get; set; }
+    }
+}
