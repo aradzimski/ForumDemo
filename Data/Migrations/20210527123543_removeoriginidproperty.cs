ef846d9198efbcbf1df1f19f019bdf079f855c43
aradzimski <adrianradzimski@gmail.com>
2021-05-27 14:35:55 +0200

    Removed origin post id property for now, looking for better replacement

---
+++ Data/Migrations/20210527123543_removeoriginidproperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumDemo.Data.Migrations
+{
+    public partial class removeoriginidproperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginPostId",
+                table: "Topics");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OriginPostId",
+                table: "Topics",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
