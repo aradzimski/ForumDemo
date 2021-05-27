f5d095a7bb38aa4c09aa6a04ba98e7c306c69171
aradzimski <adrianradzimski@gmail.com>
2021-05-27 14:00:33 +0200

    ViewModel for topic creation

---
+++ ViewModels/Main/CreateTopicViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ForumDemo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumDemo.ViewModels.Main
+{
+    public class CreateTopicViewModel
+    {
+        public int ForumId { get; set; }
+        public string ForumTitle { get; set; }
+
+        [Display(Name="Topic title")]
+        [Required]
+        [MinLength(2)]
+        public string Title { get; set; }
+
+        [Display(Name = "Topic description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Post contents")]
+        [Required]
+        [MinLength(2)]
+        public string Contents { get; set; }
+    }
+}
