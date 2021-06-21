6b3dd7052c1f9f7be1f0e5a62cefd1312f2379d0
aradzimski <adrianradzimski@gmail.com>
2021-06-21 12:49:04 +0200

    Defined post routing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,20 +21,12 @@ namespace ForumDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<User> _userManager;
-        private readonly UserRepository _userRepository;
         private readonly ForumRepository _forumRepository;
-        private readonly TopicRepository _topicRepository;
-        private readonly PostRepository _postRepository;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager,UserRepository userRepository, ForumRepository forumRepository, TopicRepository topicRepository, PostRepository postRepository)
+        public HomeController(ILogger<HomeController> logger, ForumRepository forumRepository)
         {
             _logger = logger;
-            _userManager = userManager;
-            _userRepository = userRepository;
             _forumRepository = forumRepository;
-            _topicRepository = topicRepository;
-            _postRepository = postRepository;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -52,52 +44,5 @@ namespace ForumDemo.Controllers
 
             return View(vm);
         }
-
-        [Authorize]
-        public async Task<IActionResult> EditPost(int id)
-        {
-            Post post = await _postRepository.GetById(id);
-
-            EditPostViewModel vm = new EditPostViewModel()
-            {
-                Post = await _postRepository.GetById(id),
-                Contents = post.Contents
-            };
-
-            // Manually set breadcrumb nodes
-            var childNode1 = new MvcBreadcrumbNode("Forum", "Home", vm.Post.Topic.Forum.Title)
-            {
-                RouteValues = new { id = vm.Post.Topic.Forum.Id }
-            };
-            var childNode2 = new MvcBreadcrumbNode("Topic", "Home", vm.Post.Topic.Title)
-            {
-                RouteValues = new { id = vm.Post.Topic.Id },
-                OverwriteTitleOnExactMatch = true,
-                Parent = childNode1
-            };
-            var childNode3 = new MvcBreadcrumbNode("Editing post", "Home", "Editing post")
-            {
-                Parent = childNode2
-            };
-
-            ViewData["BreadcrumbNode"] = childNode3;
-
-            return View(vm);
-        }
-
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> EditPost(EditPostViewModel vm)
-        {
-            User user = await _userManager.GetUserAsync(HttpContext.User);
-
-            Post post = await _postRepository.GetById(vm.Post.Id);
-            post.Contents = vm.Contents;
-
-            await _postRepository.Update(post);
-
-            return RedirectToAction("Topic", new { id = vm.Post.Topic.Id });
-        }
-
     }
 }
